f714a2de7610d06ae277f08361de3ca7072605d1
jean luc tuyishime <ltuyishime@exuus.com>
2020-02-18 21:25:54 +0200

    setup material ui (#5)

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { createAppContainer } from 'react-navigation';
+import { Provider as PaperProvider } from 'react-native-paper';
 import AppNavigator from './src/navigation/AppNavigator';
 
 const AppContainer = createAppContainer(AppNavigator);
 
-const App = () => <AppContainer />;
+const App = () => (
+  <PaperProvider>
+    <AppContainer />
+  </PaperProvider>
+);
 
 export default App;
